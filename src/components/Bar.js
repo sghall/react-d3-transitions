682f1eac9430f1ec99f30d67026ec77743fb5af9
Steve Hall <hall.steven.g@gmail.com>
2016-05-03 14:12:16 -0700

    separate functions for mounting, updating, and removing

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -9,12 +9,16 @@ const percentFormat = format('.2%');
 export class Bar extends Component {
 
   componentDidMount() {
-    let {props: {data: {yVal, size: [w, h]}}, refs: {node, rect, text}} = this;
+    this.isMounting.call(this, this.props, this.refs);
+  }
+
+  isMounting(props, refs) {
+    let {node, rect, text} = refs;
+    let {data: {yVal, size: [w, h]}} = props;
 
     rect.setAttribute('width', w);
     rect.setAttribute('height', h);
     text.setAttribute('x', w - 3);
-
 
     let interp0 = interpolateTransformSvg('translate(0,500)', `translate(0,${yVal})`);
     let interp1 = interpolateNumber(1e-6, 1);
@@ -29,58 +33,58 @@ export class Bar extends Component {
     });
   }
 
+  isUpating(props, next, refs) {
+    let {data: {yVal, size: [w, h]}} = props;
+
+    let interp0 = interpolateTransformSvg(`translate(0,${yVal})`, `translate(0,${next.data.yVal})`);
+    let interp1 = interpolateNumber(w, next.data.size[0]);
+    let interp2 = interpolateNumber(h, next.data.size[1]);
+    let interp3 = interpolateNumber(w - 3, next.data.size[0] - 3);
+
+    refs.node.setAttribute('opacity', 1);
+
+    this.transition = timer(elapsed => {
+      let t = elapsed < duration ? (elapsed / duration): 1;
+      refs.node.setAttribute('transform', interp0(t));
+      refs.rect.setAttribute('width', interp1(t));
+      refs.rect.setAttribute('height', interp2(t));
+      refs.text.setAttribute('x', interp3(t));
+      if (t === 1) {
+        this.transition.stop();
+      }
+    });
+  }
+
+  isRemoving(props, refs) {
+    let {data: {yVal, udid}, removeNode} = props;
+
+    let interp0 = interpolateTransformSvg(`translate(0,${yVal})`, 'translate(0,500)');
+    let interp1 = interpolateNumber(1, 1e-6);
+
+    this.transition = timer(elapsed => {
+      let t = elapsed < duration ? (elapsed / duration): 1;
+      refs.node.setAttribute('transform', interp0(t));
+      refs.node.setAttribute('opacity', interp1(t));
+      if (t === 1) {
+        this.transition.stop();
+        removeNode(udid);
+      }
+    });
+  }
+
   componentWillReceiveProps(next) {
-    let {props: {data: {yVal, udid, size: [w, h]}, removeNode}, refs: {node, rect, text}} = this;
+    let { props, refs } = this;
 
     this.transition.stop();
 
     if (next.data.type === 'UPDATING') {
-      let interp0 = interpolateTransformSvg(`translate(0,${yVal})`, `translate(0,${next.data.yVal})`);
-      let interp1 = interpolateNumber(w, next.data.size[0]);
-      let interp2 = interpolateNumber(h, next.data.size[1]);
-      let interp3 = interpolateNumber(w - 3, next.data.size[0] - 3);
-
-      node.setAttribute('opacity', 1);
-
-      this.transition = timer(elapsed => {
-        let t = elapsed < duration ? (elapsed / duration): 1;
-        node.setAttribute('transform', interp0(t));
-        rect.setAttribute('width', interp1(t));
-        rect.setAttribute('height', interp2(t));
-        text.setAttribute('x', interp3(t));
-        if (t === 1) {
-          this.transition.stop();
-        }
-      });
+      this.isUpating.call(this, props, next, refs);
     } else if (next.data.type === 'MOUNTING') {
-      rect.setAttribute('width', next.data.size[0]);
-      rect.setAttribute('height', next.data.size[1]);
-      text.setAttribute('x', next.data.size[0] - 3);
-
-      let interp0 = interpolateTransformSvg('translate(0,500)', `translate(0,${next.data.yVal})`);
-      let interp1 = interpolateNumber(1e-6, 1);
-
-      this.transition = timer(elapsed => {
-        let t = elapsed < duration ? (elapsed / duration): 1;
-        node.setAttribute('transform', interp0(t));
-        node.setAttribute('opacity', interp1(t));
-        if (t === 1) {
-          this.transition.stop();
-        }
-      });
-    } else { // REMOVING
-      let interp0 = interpolateTransformSvg(`translate(0,${yVal})`, 'translate(0,500)');
-      let interp1 = interpolateNumber(1, 1e-6);
-
-      this.transition = timer(elapsed => {
-        let t = elapsed < duration ? (elapsed / duration): 1;
-        node.setAttribute('transform', interp0(t));
-        node.setAttribute('opacity', interp1(t));
-        if (t === 1) {
-          this.transition.stop();
-          removeNode(udid);
-        }
-      });
+      this.isMounting.call(this, next, refs);
+    } else if (next.data.type === 'REMOVING') {
+      this.isRemoving.call(this, props, refs);
+    } else {
+      throw new Error('Invalid Node Type');
     }
   }
 
