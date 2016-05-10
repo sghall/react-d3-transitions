59396c28862a3feafbd0d8d89ef2a45b80bc3276
Steve Hall <hall.steven.g@gmail.com>
2016-05-10 08:29:40 -0700

    remove github banner

--- examples/streamGraphExample/src/main.js
+++ examples/streamGraphExample/src/main.js
@@ -18,13 +18,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <a href='https://github.com/sghall'>
-          <img 
-            style={{position: 'absolute', top: 0, right: 0, border: 0}}
-            src='https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67' 
-            alt='Fork me on GitHub' 
-          />
-          </a>
         <Example />
       </div>
     );
